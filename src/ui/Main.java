08067efe68f6381c82bec151385e83bd93c0cea7
davidps79 <1006352045@u.icesi.edu.co>
2022-05-23 16:51:19 -0500

    Graph implementations from integrator homework 3

---
+++ src/ui/Main.java
@@ -0,0 +1,14 @@
+package ui;
+
+import dataStructures.ListGraph;
+import dataStructures.MatrixGraph;
+
+public class Main {
+	public static void main(String[] args) {
+		Main main = new Main(2);
+	}
+	
+	public Main() {
+		
+	}
+}
